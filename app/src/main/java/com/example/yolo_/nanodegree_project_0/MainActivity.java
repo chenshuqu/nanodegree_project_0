17002988f8297758f9b3cabfec71f7e7c75ebb74
ChenSven <yolo_sven@126.com>
2016-06-15 22:11:18 +0800

    Initial Project

---
+++ app/src/main/java/com/example/yolo_/nanodegree_project_0/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.yolo_.nanodegree_project_0;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    String tag = "Lifecycle";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user touches the button */
+    public void onClick_btn_P0(View view) {
+        switch(view.getId()){
+            case R.id.btn_P0_popularMovie:
+                // Do something in response to button click;
+                Toast.makeText(getApplicationContext(),"This button will lauch my popularMovie app.",Toast.LENGTH_SHORT).show();
+            break;
+            case R.id.btn_P0_stockHawk:
+                // Do something in response to button click;
+                Toast.makeText(getApplicationContext(),"This button will lauch my stockhawk app.",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_P0_buildItBigger:
+                // Do something in response to button click;
+                Toast.makeText(getApplicationContext(),"This button will lauch my BUILD IT BIGGER app.",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_P0_capstone:
+                // Do something in response to button click;
+                Toast.makeText(getApplicationContext(),"This button will lauch my capstone app.",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_P0_makeYourAppMaterial:
+                // Do something in response to button click;
+                Toast.makeText(getApplicationContext(),"This button will lauch my MAKE_YOUR_APP_MATERIAL app.",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_P0_goUbiquitous:
+                // Do something in response to button click;
+                Toast.makeText(getApplicationContext(),"This button will lauch my GO UBIQUITOUS app.",Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    public void onStart()
+    {
+        super.onStart();
+        Log.d(tag,"In the onStart() event");
+    }
+}
---
+++ app/src/test/java/com/example/yolo_/nanodegree_project_0/MainActivityTest.java
@@ -0,0 +1,10 @@
+package com.example.yolo_.nanodegree_project_0;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by yolo_ on 2016/6/15.
+ */
+public class MainActivityTest {
+
+}
